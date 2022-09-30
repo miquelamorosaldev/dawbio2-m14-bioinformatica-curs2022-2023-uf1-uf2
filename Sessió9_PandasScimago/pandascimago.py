b8ae73c2d19e7328ed492fb3e5154000c274a533
Miquel Amorós <mamoro10@xtec.cat>
2022-09-30 17:24:05 +0200

    Pandas Scimago, codi font.

--- Sessió9_PandasScimago/pandascimago.py
+++ Sessió9_PandasScimago/pandascimago.py
@@ -1,6 +1,55 @@
 import numpy as np
 import pandas as pd
+import copy
+
+csv_file_path = "./Sessió9_PandasScimago/scimago-medicine.csv"
 
 # Read scimago ranking
-entries: pd.DataFrame = pd.read_csv("./Sessió9_PandasScimago/scimagomedicine.csv", sep=";")
+entries: pd.DataFrame = pd.read_csv(csv_file_path, sep=";")
 print(entries)
+
+#seleccionar i mostrar les entries amb H index superior
+entries_high = entries.loc[:,"H index"] >= 450
+entries_ok = entries.loc[entries_high,:]
+
+#Provem que ha funcionat
+print(entries_ok.loc[:,["Title","H index"]])
+
+
+# Amb el loc també podem editar files del dataframe.
+
+#canviar totes les entrades menors de 750 a h_index negatiu
+entries2 = copy.deepcopy(entries)
+
+# Màscara per a seleccionar només els que tenen menys de 750
+bad_entries_mask = (entries2.loc[:,"H index"] < 750)
+
+# Aquesta és la lína que edita els H Index, els posa un 0. 
+entries2.loc[bad_entries_mask,"H index"] = 0;
+
+# Ordena els valors i mostra els 10 primers.
+entries2 = entries2.sort_values(by=["H index"], 
+                          axis=0, 
+                          ascending=False).head(10)
+
+print("Canvi valors.")
+#Provem que ha funcionat
+print(entries2.loc[:,["Title","H index"]])
+
+# Canviem el tipus de publicació, de Journal a Diari.
+
+# Opcions de mask.
+journals = entries2.loc[:,"Type"] == 'journal'
+#journals = entries2.loc[:,"Type"].str.contains('journal')
+print(journals.head(100))
+
+# Aquesta és la lína que edita els H Index, els posa un 0. 
+entries2.loc[journals,"Type"] = 'Diari';
+
+
+print(entries2.loc[:,["Title","Type"]])
+
+# També ho podem fer amb el replace.
+
+entries3 = entries2.replace('journal','diari')
+print(entries3.loc[:,["Title","Type"]])
